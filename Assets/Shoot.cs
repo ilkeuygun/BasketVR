b3dba3c5d8bf76bb967269003b73389e6d5ddbc2
Onur Alsaran <onuralsaran@gmail.com>
2016-04-29 17:28:16 +0300

    Ball throwing is added, Player height is changed

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Shoot : MonoBehaviour {
 
+	public Transform handTransform;
+	public GameObject ball;
+
 	// Use this for initialization
 	void Start () {
 		Physics.gravity = new Vector3 (0, -120, 0);
@@ -10,6 +13,16 @@ public class Shoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject currentBall = (GameObject)Instantiate (ball, handTransform.position, Quaternion.identity);
+
+			Vector3 direction = new Vector3 () {
+				x = handTransform.forward.x,
+				y = handTransform.forward.y + 0.3f,
+				z = handTransform.forward.z,
+			};
+
+			currentBall.GetComponent<Rigidbody> ().AddForce (direction * 10000);
+		}
 	}
 }
